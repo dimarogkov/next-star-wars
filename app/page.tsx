d54fd71d1f750579a2dfcf09baa8691ad6750306
dima.rogkov <dimarogkov41475@ukr.net>
2024-07-25 14:33:22 +0300

    add pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+import { useQuery } from '@tanstack/react-query';
+import { getPeople } from '@/src/services/people';
 import PeopleList from '@/src/components/elements/PeopleList/PeopleList';
+import Pagination from '@/src/components/elements/Pagination/Pagination';
+import { Loader } from '@/src/components/ui';
 
 const HomePage = () => {
+    const { data, isLoading } = useQuery({
+        queryFn: () => getPeople(),
+        select: (data) => data.data,
+        queryKey: ['people'],
+    });
+
     return (
         <section className='relative w-full'>
-            <PeopleList />
+            {isLoading && <Loader />}
+
+            {data && (
+                <>
+                    <Pagination peopleCount={data.count} getPeople={getPeople} />
+                    <PeopleList people={data.results} />
+                </>
+            )}
         </section>
     );
 };
